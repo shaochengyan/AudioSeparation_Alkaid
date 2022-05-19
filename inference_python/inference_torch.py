f2c8d2feaf516d0dc142048a0bfe539913148aa2
colaCheng <colachengcheng@gmail.com>
2022-05-19 17:45:21 +0800

    add torch model

--- inference_python/inference_torch.py
+++ inference_python/inference_torch.py
@@ -3,17 +3,23 @@ from asteroid.models import ConvTasNet
 import yaml
 
 # get the convtasnet model with configuration in yaml file
-def get_model(conf_filepath: str):
+def get_model(conf_filepath: str, ckpt_path=None):
     with open("./conf/conf_model1.yml") as f:
         conf = yaml.safe_load(f)
     model = ConvTasNet(
         **conf["filterbank"], **conf["masknet"], sample_rate=16000
     )
+    
+    # load checkpoint 
+    if ckpt_path != None:
+        best_state_dict = torch.load(ckpt_path)['state_dict']
+        model.load_state_dict(best_state_dict)
+    
     return model
 
 if __name__ == '__main__':
-    model1 = get_model("./conf/conf_model1.yml")
-    # model2 = get_model("./conf/conf_model2.yml")
+    model1 = get_model("./conf/conf_model1.yml", "model_torch/model1_torch.pth")
+    # model2 = get_model("./conf/conf_model2.yml", "model_torch/model1_torch.pth")
     
     # dummy input music
     dummy_input = torch.rand(size=(16000, ))
